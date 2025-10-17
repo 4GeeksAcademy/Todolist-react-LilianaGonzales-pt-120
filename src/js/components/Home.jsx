138dbc90de7038fc1535c1b2d3d66fc5d06b2ece
lili14gon <liligh14@gmail.com>
2025-10-17 18:42:53 +0200

    primer avance

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+const [nuevaTarea, setnuevaTarea] = useState('')
+const [tareas, setTareas] = useState([])
+
+
+const handleChange=(event)=>{
+	
+ console.log(event);
+ setnuevaTarea(event.target.value);
+ console.log(nuevaTarea);
+}
+
+const handleSubmit =(e)=>{
+	e.preventDefault();
+	setTareas([...tareas,nuevaTarea])
+	setnuevaTarea('')
+}
 	return (
+		<form onSubmit={handleSubmit}>
 		<div className="text-center">
             
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Todos</h1>
+			<input type="text" onChange={handleChange} value={nuevaTarea}/>
+			{/* <input type="text"  defaultValue={tarea} onKeyDown={handleChange}/> */}
+			
+			{/* <div>{tarea}</div> */}
+			<ul>
+        {tareas.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
 		</div>
+		</form>
 	);
 };
 
